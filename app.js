01bf432839779781e457ed7ec3088f21b928d795
ci6803 <ci6803@gmail.com>
2022-08-09 21:52:49 +0200

    done

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // ...
 
@@ -22,4 +22,18 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => res.render('beers', { beersFromApi }))
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromApi => res.render('random-beer', { responseFromApi }))
+    .catch(error => console.log(error));
+});
+
+app.listen(3006, () => console.log('🏃‍ on port 3006'));
